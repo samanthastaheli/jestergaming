f7f092a8c0802d7257df9d665f7ee787d4e2d575
Samantha Staheli <sstaheli27@gmail.com>
2025-11-21 12:44:31 -0700

    cleaned code

--- run_live_stream.py
+++ run_live_stream.py
@@ -1,5 +1,10 @@
+"""
+Run model in live stream mode using web cam image as input.
+"""
+
 import cv2
 import time
+import argparse
 import pyautogui
 import numpy as np
 import mediapipe as mp
@@ -7,23 +12,35 @@ from mediapipe.tasks.python import vision
 from mediapipe.tasks import python
 from mediapipe.framework.formats import landmark_pb2
 
-# Path to your .task model
+# Path to .task model
 MODEL_PATH = "exported_model/gesture_recognizer.task"
 
 QUIT_KEY = 'q'
+
+# Update in callbacks
+MOTION = None
+ACTION = None
+
+# region Async Callback Functions 
 
 def motion_callback(result, output_image, timestamp_ms):
       """This will be called every time results are ready."""
       if result.gestures:
             gesture = result.gestures[0][0]   # Top gesture
+            MOTION = gesture.category_name
             print(f"[{timestamp_ms}] Motion Gesture: {gesture.category_name} (score={gesture.score:.2f})")
+
 
 def action_callback(result, output_image, timestamp_ms):
       """This will be called every time results are ready."""
       if result.gestures:
             gesture = result.gestures[0][0]   # Top gesture
+            ACTION = gesture.category_name
             print(f"[{timestamp_ms}] Action Gesture: {gesture.category_name} (score={gesture.score:.2f})")
 
+# endregion
+
+# region Helper Functions
 
 def resize_with_aspect_ratio(image, target_width, target_height):
     """
@@ -42,7 +59,35 @@ def resize_with_aspect_ratio(image, target_width, target_height):
     return canvas
 
 
-def main():
+def frame_to_mp_image(frame):
+      frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+      mp_frame = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame_rgb)
+      return mp_frame
+
+
+def split_frame(frame, hand_type):
+      frame = cv2.flip(frame, 1) # flip to have correct right/left sides
+
+      _, w = frame.shape[:2]
+
+      # Split into left and right halves
+      left_half = frame[:, :w//2]
+      right_half = frame[:, w//2:]
+      if hand_type.lower() == "r":
+            motion_image = frame_to_mp_image(left_half)
+            action_image = frame_to_mp_image(right_half)
+      if hand_type.lower() == "l":
+            motion_image = frame_to_mp_image(left_half)
+            action_image = frame_to_mp_image(right_half)
+
+      return motion_image, action_image
+
+# endregion
+
+# region main
+
+def main(hand_type):
+      # Get models
       base_options = python.BaseOptions(model_asset_path=MODEL_PATH)
 
       # Configure recognizer for live stream mode
@@ -73,21 +118,18 @@ def main():
 
       while cap.isOpened():
             success, frame = cap.read()   
-            frame = cv2.flip(frame, 1) # flip to have correct right/left sides
 
             if not success:
                   break
 
+            motion_image, action_image = split_frame(frame, hand_type)
 
-            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            mp_frame = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame_rgb)
-            
             timestamp = int(time.time() * 1000)   # ms
-            motion_recognizer.recognize_async(mp_frame, timestamp)
-            action_recognizer.recognize_async(mp_frame, timestamp)
+            motion_recognizer.recognize_async(motion_image, timestamp)
+            action_recognizer.recognize_async(action_image, timestamp)
 
             # Resize to correct aspect ratio 
-            win_x, win_y, win_w, win_h = cv2.getWindowImageRect("Gesture Recognition")
+            _, _, win_w, win_h = cv2.getWindowImageRect("Gesture Recognition")
             frame_display = resize_with_aspect_ratio(frame, win_w, win_h)
 
             # Display image in cv2 window 
@@ -102,5 +144,22 @@ def main():
       action_recognizer.close()
       cv2.destroyAllWindows()
 
+
 if __name__ == "__main__":
-    main()
+      # Argument Parser
+      parser = argparse.ArgumentParser(description="Hand gesture recognizer arguments.")
+
+      parser.add_argument(
+            "--handedness",
+            "-H",
+            required=False,
+            default="r",
+            choices=['l', 'r', 'L', 'R'],
+            help="'l' or 'r' handedness"
+      )
+
+      args = parser.parse_args()
+
+      main(args.handedness)
+
+# endregion
